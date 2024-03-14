94dc9fb9f6fc7f097897e521301f36b503a1602e
andriusLedas <152397675+andriusLedas@users.noreply.github.com>
2024-03-14 15:54:14 +0200

    Add H2 DB and TestController

---
+++ src/main/java/com/swedbank/StudentApp/TestController.java
@@ -0,0 +1,15 @@
+package com.swedbank.StudentApp;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/test")
+public class TestController {
+
+    @GetMapping
+    public String justTest() {
+        return "Test output";
+    }
+}
